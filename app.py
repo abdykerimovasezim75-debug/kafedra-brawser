d521599152139a170ec4c691b7eeab906c67e08a
kamillovnas <d2f9jcndtd@privaterelay.appleid.com>
2026-05-28 12:39:59 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 app.secret_key = "12345"
+
+UPLOAD_FOLDER = "static/uploads"
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+# максимум 2 MB
+app.config["MAX_CONTENT_LENGTH"] = 2 * 1024 * 1024
 
 
 @app.route("/")
@@ -13,6 +24,27 @@ def home():
 @app.route("/test")
 def test():
     return render_template("test.html")
+
+
+@app.route("/reviews", methods=["GET", "POST"])
+def reviews():
+
+    if request.method == "POST":
+
+        photo = request.files["photo"]
+
+        if photo:
+
+            photo.save(
+                os.path.join(
+                    app.config["UPLOAD_FOLDER"],
+                    photo.filename
+                )
+            )
+
+            return "Фото загружено"
+
+    return render_template("reviews.html")
 
 
 if __name__ == "__main__":
