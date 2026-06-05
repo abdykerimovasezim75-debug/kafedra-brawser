0b454aa98a973db3247aeb90e50ffc65209f4c9a
fama <famadersa@gmail.com>
2026-06-06 01:10:54 +0600

    Add files via upload

---
+++ update_admin.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+
+conn = sqlite3.connect("database/database.db")
+cursor = conn.cursor()
+
+
+cursor.execute("SELECT * FROM users")
+
+
+for user in cursor.fetchall():
+    print(user)
+
+
+conn.close()
