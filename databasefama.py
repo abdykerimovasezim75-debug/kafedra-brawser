3da621491865ba003ce9d40a2cb433e834ee29f6
fama <famadersa@gmail.com>
2026-06-06 02:04:55 +0600

    Add files via upload

---
+++ databasefama.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+conn = sqlite3.connect("databasefama.db")
+cursor = conn.cursor()
+
+# таблица users
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    password TEXT,
+    role TEXT
+)
+""")
+
+
+# таблица reviews
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS reviews (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    text TEXT,
+    date TEXT,
+    likes INTEGER DEFAULT 0,
+    photo TEXT
+)
+""")
+
+# тестовый пользователь
+cursor.execute(
+    "INSERT INTO users (username, password, role) VALUES (?, ?, ?)",
+    ("admin", "1234", "admin")
+)
+
+conn.commit()
+conn.close()
+
+print("База создана!")
