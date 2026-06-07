62486dca35232be7149a8badab5f57126c1db707
Fatima <fatimaiskenderova29@gmail.com>
2026-06-08 02:43:26 +0600

    Update app.py

--- app.py
+++ app.py
@@ -257,5 +257,8 @@ def result():
                            show_razbor=show_razbor, 
                            report=report)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
